e6e92ab2fe61de1f28e3619c80e954c1256bd7fd
LorenMucha <lorenmucha@gmail.com>
2023-10-17 11:47:36 +0200

    fix: legend ui

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -3,7 +3,11 @@ import { TabElem } from "./model/tabs-elem";
 
 export const MENU: Map<TabElem, Array<IndicatorImpl>> = new Map<TabElem, Array<IndicatorImpl>>([
     [{ title: "Marktdaten", active: true },
-    [new IndicatorImpl('kjhdkJAHKHKJASFAKhdJKDKJSHDKAHJKAHDHASKJ', 'zu_fortzuege.json', 'Zu und Fortzüge'), new IndicatorImpl('', '', 'Auspendler'), new IndicatorImpl('', '', 'Einpendler')]],
+    [new IndicatorImpl('Culpa culpa est duis ipsum ut eu sint adipisicing id eiusmod elit laborum aliqua nulla.Sunt in ullamco commodo duis minim exercitation magna tempor.Commodo id duis laborum mollit quis ut irure est.',
+        'zu_fortzuege.json',
+        'Zu und Fortzüge'),
+    new IndicatorImpl('', '', 'Auspendler'),
+    new IndicatorImpl('', '', 'Einpendler')]],
     [{ title: "Mieten", active: false },
     [new IndicatorImpl('', '', 'Anzahl Angebote'), new IndicatorImpl('', '', 'arith. Mittel €/m²')]]
 ]);
